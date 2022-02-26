3d8c29c9f47e28de239fcfd8ba69341e7ecd37d7
Danielies367 <danielkristioutomo@gmail.com>
2022-02-26 20:43:48 +0700

    adding detection function
    along with learn and train model

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-from re import A
 from neuralintents import GenericAssistant
 import speech_recognition as sr
 import pyttsx3 as tts
 import datetime
 import sys
 from datetime import datetime
-from telegram import EncryptedPassportElement
 import wikipedia
 import webbrowser
 import os
 import smtplib
 from pywikihow import search_wikihow
-import keyboard
 import time
 import requests
 from bs4 import BeautifulSoup
@@ -22,8 +19,27 @@ import pyautogui
 import pyjokes
 import random
 import hashlib
-import playsound
-import threading
+import mediapipe as mp
+from Trainmodel import TrainMod
+from Learnmodel import LearnMod
+import cv2  # Import opencv
+import csv
+import os
+import numpy as np
+import pandas as pd
+import speech_recognition as sr
+import pickle
+import pandas as pd
+
+mp_drawing = mp.solutions.drawing_utils  # Drawing helpers
+mp_holistic = mp.solutions.holistic  # Mediapipe Solutions
+cap = cv2.VideoCapture(0)
+
+with open(
+    "D:\Eagleies\SLAVIK AI Project\slavik\BodyLanguageDecoder/body_language.pkl", "rb"
+) as f:
+    model = pickle.load(f)
+
 
 recognizer = sr.Recognizer()
 speaker = tts.init()
@@ -32,7 +48,6 @@ speaker.setProperty("rate", 170)
 voices = speaker.getProperty("voices")  # gets you the details of the current voices
 speaker.setProperty("voice", voices[1].id)
 gsearch = search
-cam = cv2.VideoCapture(3)
 
 todo_list = ["Work", "Learn", "Sleep"]
 schedule_list = [
@@ -647,17 +662,23 @@ def listenss():
 
 def camera():
 
-    vid = cv2.VideoCapture(0)
+    ################################
+    wCam, hCam = 1280, 720
+    ################################
+
+    cap = cv2.VideoCapture(0)
+    cap.set(3, wCam)
+    cap.set(4, hCam)
 
     while True:
 
         # Capture the video frame
         # by frame
-        ret, frame = vid.read()
+        ret, frame = cap.read()
 
         # Display the resulting frame
-        cv2.imshow("frame", frame)
-
+        cv2.imshow("Your Pretty Face", frame)
+        func()
         # the 'q' button is set as the
         # quitting button you may use any
         # desired button of your choice
@@ -665,7 +686,7 @@ def camera():
             break
 
     # After the loop release the cap object
-    vid.release()
+    cap.release()
     # Destroy all the windows
     cv2.destroyAllWindows()
 
@@ -674,14 +695,21 @@ def camera():
 
 
 def send_mail():
-    email = "danielkri.photography@gmail.com"
+    riomail = "rionardosupardy123@gmail.com"
+    arlenmail = "hadrianreith12@gmail.com"
+    email = "danielkris.photography@gmail.com"
     password = "gendatzbesar"
     speak("what is the email address you want to send the message to")
-    to = "rionardosupardy123@gmail.com"
+    query = takecommand()
+    if "rio" in query:
+        to = riomail
+    else:
+        to = arlenmail
+
     speak("what is the subject of the message")
     subject = "test"
     speak("what is the message you want to send")
-    message = "ngetest kecerdasan rio"
+    message = takecommand()
     speak("sending mail")
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -692,6 +720,230 @@ def send_mail():
         speak("mail sent successfully")
     except:
         speak("mail not sent")
+
+
+def DetectModel():
+    wCam, hCam = 1280, 720
+    cap = cv2.VideoCapture(0)
+    cap.set(3, wCam)
+    cap.set(4, hCam)
+    # Initiate holistic model
+    with mp_holistic.Holistic(
+        min_detection_confidence=0.5, min_tracking_confidence=0.5
+    ) as holistic:
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+
+            # Recolor Feed
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+
+            # Make Detections
+            results = holistic.process(image)
+            # print(results.face_landmarks)
+
+            # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
+
+            # Recolor image back to BGR for rendering
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            # 1. Draw face landmarks
+            mp_drawing.draw_landmarks(
+                image,
+                results.face_landmarks,
+                mp_holistic.FACEMESH_TESSELATION,
+                mp_drawing.DrawingSpec(
+                    color=(80, 110, 10), thickness=1, circle_radius=1
+                ),
+                mp_drawing.DrawingSpec(
+                    color=(80, 256, 121), thickness=1, circle_radius=1
+                ),
+            )
+
+            # 2. Right hand
+            mp_drawing.draw_landmarks(
+                image,
+                results.right_hand_landmarks,
+                mp_holistic.HAND_CONNECTIONS,
+                mp_drawing.DrawingSpec(
+                    color=(80, 22, 10), thickness=2, circle_radius=4
+                ),
+                mp_drawing.DrawingSpec(
+                    color=(80, 44, 121), thickness=2, circle_radius=2
+                ),
+            )
+
+            # 3. Left Hand
+            mp_drawing.draw_landmarks(
+                image,
+                results.left_hand_landmarks,
+                mp_holistic.HAND_CONNECTIONS,
+                mp_drawing.DrawingSpec(
+                    color=(121, 22, 76), thickness=2, circle_radius=4
+                ),
+                mp_drawing.DrawingSpec(
+                    color=(121, 44, 250), thickness=2, circle_radius=2
+                ),
+            )
+
+            # 4. Pose Detections
+            mp_drawing.draw_landmarks(
+                image,
+                results.pose_landmarks,
+                mp_holistic.POSE_CONNECTIONS,
+                mp_drawing.DrawingSpec(
+                    color=(245, 117, 66), thickness=2, circle_radius=4
+                ),
+                mp_drawing.DrawingSpec(
+                    color=(245, 66, 230), thickness=2, circle_radius=2
+                ),
+            )
+            # Export coordinates
+            try:
+                # Extract Pose landmarks
+                pose = results.pose_landmarks.landmark
+                pose_row = list(
+                    np.array(
+                        [
+                            [landmark.x, landmark.y, landmark.z, landmark.visibility]
+                            for landmark in pose
+                        ]
+                    ).flatten()
+                )
+
+                # Extract Face landmarks
+                face = results.face_landmarks.landmark
+                face_row = list(
+                    np.array(
+                        [
+                            [landmark.x, landmark.y, landmark.z, landmark.visibility]
+                            for landmark in face
+                        ]
+                    ).flatten()
+                )
+
+                # Concate rows
+                row = pose_row + face_row
+
+                #             # Append class name
+                #             row.insert(0, class_name)
+
+                #             # Export to CSV
+                #             with open('coords.csv', mode='a', newline='') as f:
+                #                 csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                #                 csv_writer.writerow(row)
+
+                # Make Detections
+                X = pd.DataFrame([row])
+                body_language_class = model.predict(X)[0]
+                body_language_prob = model.predict_proba(X)[0]
+                print(f"{body_language_class}")
+
+                classname = body_language_class
+                for i in classname:
+                    if i == "Happy":
+                        print("you are happy")
+                        speak("you are happy")
+                    elif i == "sad":
+                        print("your face is sad")
+                        speak("your face is sad")
+
+                # Grab ear coords
+                coords = tuple(
+                    np.multiply(
+                        np.array(
+                            (
+                                results.pose_landmarks.landmark[
+                                    mp_holistic.PoseLandmark.LEFT_EAR
+                                ].x,
+                                results.pose_landmarks.landmark[
+                                    mp_holistic.PoseLandmark.LEFT_EAR
+                                ].y,
+                            )
+                        ),
+                        [640, 480],
+                    ).astype(int)
+                )
+
+                cv2.rectangle(
+                    image,
+                    (coords[0], coords[1] + 5),
+                    (coords[0] + len(body_language_class) * 20, coords[1] - 30),
+                    (245, 117, 16),
+                    -1,
+                )
+                cv2.putText(
+                    image,
+                    body_language_class,
+                    coords,
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 255, 255),
+                    2,
+                    cv2.LINE_AA,
+                )
+
+                # Get status box
+                cv2.rectangle(image, (0, 0), (250, 60), (245, 117, 16), -1)
+
+                # Display Class
+                cv2.putText(
+                    image,
+                    "CLASS",
+                    (95, 12),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (0, 0, 0),
+                    1,
+                    cv2.LINE_AA,
+                )
+                cv2.putText(
+                    image,
+                    body_language_class.split(" ")[0],
+                    (90, 40),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 255, 255),
+                    2,
+                    cv2.LINE_AA,
+                )
+
+                # Display Probability
+                cv2.putText(
+                    image,
+                    "PROB",
+                    (15, 12),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (0, 0, 0),
+                    1,
+                    cv2.LINE_AA,
+                )
+                cv2.putText(
+                    image,
+                    str(round(body_language_prob[np.argmax(body_language_prob)], 2)),
+                    (10, 40),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 255, 255),
+                    2,
+                    cv2.LINE_AA,
+                )
+
+            except:
+                pass
+
+            cv2.imshow("Raw Webcam Feed", image)
+
+            if cv2.waitKey(10) or 0xFF == ord("q"):
+                break
+
+
+cap.release()
+cv2.destroyAllWindows()
+time.sleep(1)
 
 
 def func():
@@ -980,15 +1232,37 @@ def func():
         elif "generate random number" in query or "random number" in query:
             random_number()
 
+        elif "train my model" in query:
+            speak("wait a second")
+            TrainMod()
+            speak("your model have been trained")
+
+        elif "learn my model" in query:
+            speak("what class name for your model sir")
+            class_name = takecommand().lower()
+            speak("okay sir, please wait a moment")
+            LearnMod(f"{class_name}")
+
+        elif "open detection" in query:
+            speak("opening detection")
+            speak("wait a second")
+            DetectModel()
+
+        elif (
+            "close detection" in query
+            or "stop training" in query
+            or "stop learning" in query
+        ):
+            pyautogui.press("q")
+
         elif "sleep now" in query or "sleep" in query:
             speak("okay sir, you can call me anytime!")
             break
 
 
 # enviroment()
-send_mail()
 wishme()
-func()
+DetectModel()
 
 if __name__ == "__main__":
     while True:
